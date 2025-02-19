ba8da8e4455bce4100a20eef7d247be44b09e8c7
Archive090 <neurosolod@gmail.com>
2025-02-19 15:20:01 +0200

    Create ShopCodes.lua

---
+++ ShopCodes.lua
@@ -0,0 +1,20 @@
+local Codes = {
+	[4] = {
+		Code = "test",
+		IndexId = 4,
+		Rewards = {
+			Knobs = 1,
+			Revives = 1
+		}
+	},
+	[5] = {
+		Code = "20MVISITS",
+		IndexId = 5,
+		Rewards = {
+			Knobs = 500,
+			Revives = 3,
+		},
+	}
+}
+
+return Codes
